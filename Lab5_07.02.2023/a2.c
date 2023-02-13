04f415d846cf6624c30b6f89a7a9a9013d8cd5f4
Jibesh Kumar Panda <99865910+Jibesh04@users.noreply.github.com>
2023-02-13 19:33:55 +0530

    POTD

---
+++ Lab5_07.02.2023/a2.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#define max 5
+
+char tt[4][2][max] = {{"1", "3"}, {"", "2"}, {"", ""}, {"2", ""}};
+
+int l;
+int flag = 0;
+void rec(int cst, int i, char str[]){
+    int id = 0;
+    for (; id < strlen(tt[cst][str[i] - 'a']) && i < l; id++)
+        rec(tt[cst][str[i] - 'a'][id] - '0', i + 1, str);
+
+    if (i == l && !flag && (cst == 2)){
+        printf("Accepted\n");
+        flag = 1;
+    }
+}
+
+int main(){
+    char str[30];
+    printf("Enter the string: ");
+    scanf("%s", str);
+    l = strlen(str);
+    // Function calling
+    rec(0, 0, str);
+    if (flag == 0)
+        printf("Rejected\n");
+    return 0;
+}
+
+// Terminal
+
+// PS D:\Junior\Study\Finite_Automata\Lab5_07.02.2023> gcc .\a2.c
+// PS D:\Junior\Study\Finite_Automata\Lab5_07.02.2023> .\a.exe
+// Enter the string: ab
+// Accepted
+// PS D:\Junior\Study\Finite_Automata\Lab5_07.02.2023> .\a.exe
+// Enter the string: ba
+// Accepted
+// PS D:\Junior\Study\Finite_Automata\Lab5_07.02.2023> .\a.exe
+// Enter the string: aab
+// Rejected
+// PS D:\Junior\Study\Finite_Automata\Lab5_07.02.2023> .\a.exe
+// Enter the string: bbb
+// Rejected
